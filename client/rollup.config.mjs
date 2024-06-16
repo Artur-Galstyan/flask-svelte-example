666fbbef2a223373c38a61742def17117023a1b5
Artur Galstyan <mail@arturgalstyan.dev>
2024-06-16 11:09:27 +0200

    ts support

--- client/rollup.config.mjs
+++ client/rollup.config.mjs
@@ -3,6 +3,8 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import livereload from "rollup-plugin-livereload";
 import terser from "@rollup/plugin-terser";
+import { sveltePreprocess } from "svelte-preprocess";
+import typescript from "@rollup/plugin-typescript";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -16,20 +18,16 @@ export default {
   },
   plugins: [
     svelte({
-      // enable run-time checks when not in production
-      dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file — better for performance
-      css: (css) => {
-        css.write("public/bundle.css");
+      compilerOptions: {
+        dev: !production,
+        css: (css) => {
+          css.write("public/bundle.css");
+        },
       },
+      preprocess: sveltePreprocess(),
     }),
 
-    // If you have external dependencies installed from
-    // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration —
-    // consult the documentation for details:
-    // https://github.com/rollup/rollup-plugin-commonjs
+    typescript({ sourceMap: !production }),
     nodeResolve({
       browser: true,
       dedupe: (importee) =>
@@ -37,12 +35,7 @@ export default {
     }),
     commonjs(),
 
-    // Watch the `public` directory and refresh the
-    // browser on changes when not in production
     !production && livereload("public"),
-
-    // If we're building for production (npm run build
-    // instead of npm run dev), minify
     production && terser(),
   ],
   watch: {
